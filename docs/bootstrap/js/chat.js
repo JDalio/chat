013e50d7dae0083db7c865c839e0ba7e276d8558
Dalio <2267264176@qq.com>
2018-10-27 12:19:43 +0800

    finish

--- docs/bootstrap/js/chat.js
+++ docs/bootstrap/js/chat.js
@@ -5,12 +5,13 @@ var socket = null;
 var isAuth = false;
 var userNick = null;
 var userCount = 0;
+var ws_url="ws://localhost:8000/websocket";
 $(function () {
     $("#menuModal").modal('show');
     var height = $(window).height();
     $('#content').css("height", height - $('#top').height() - $('#opt').height() - 40);
 
-    $('#loginBtn').click(function(){
+    $('#loginBtn').click(function () {
         userLogin();
     });
 
@@ -22,102 +23,124 @@ $(function () {
 
     $('#sendBtn').click(function () {
         var mess = $("#mess").val().trim();
-        if(mess){
+        if (mess) {
             sendMess(mess);
             $("#mess").val('');
         }
-    }).keyup(function(e){
+    }).keyup(function (e) {
         var keyCode = e.which || e.keyCode;
-        if(keyCode==13){
+        if (keyCode == 13) {
             $("#sendBtn").click();
         }
     });
 });
 
 function sendMess(mess) {
-    send(true, "{'code':10086,'mess':'"+mess+"'}");
-}
-;
+    send(true, "{'code':10086,'mess':'" + mess + "'}");
+};
 
 
-function userLogin() {
-    if (!userNick) {
+function userLogin()
+{
+    if (!userNick)
+    {
         userNick = $('#nick').val().trim();
     }
-    if (userNick) {
-        if (!window.WebSocket) {
+
+    if (userNick)
+    {
+        if (!window.WebSocket)
+        {
             window.WebSocket = window.MozWebSocket;
         }
-        if (window.WebSocket) {
-            window.socket = new WebSocket("ws://39.107.65.148:8000/websocket");
-            window.socket.onmessage = function (event) {
-                var data = eval("(" + event.data + ")");
+
+        if (window.WebSocket)
+        {
+
+            window.socket = new WebSocket(ws_url);
+
+            window.socket.onopen = function (event)// shake hands, build connect
+            {
+                console.log("connection success!!");
+                sendAuthInfo();
+            };
+
+            window.socket.onmessage = function (event)
+            {
+                var data = JSON.parse(event.data);
+                console.log(data);
                 console.log("onmessage data: " + JSON.stringify(data));
-                switch (data.uri) {
-                    case 1 << 8 | 220: // ping message
-                    case 2 << 8 | 220: // pong message
+                switch (data.type) {
+                    case 10015: // ping message
                         console.log("ping message: " + JSON.stringify(data));
-                        pingInvake(data);
+                        pingInvoke(data);
                         break;
-                    case 3 << 8 | 220: // system message
+                    case 988: // system message
                         console.log("system message: " + JSON.stringify(data));
-                        sysInvake(data);
+                        sysInvoke(data);
                         break;
-                    case 4 << 8 | 220: // error message
+                    case 1244: // error message
                         console.log("error message: " + JSON.stringify(data));
-                        closeInvake(null);
+                        closeInvoke(null);
                         break;
                     case 5 << 8 | 220: // auth message
-                        console.log("auth message: " + JSON.stringify(data));
-                        break;
-                    case 6 << 8 | 220: // broadcast message
+                    console.log("auth message: " + JSON.stringify(data));
+                    break;
+                    case 10086: // broadcast message
                         console.log("broadcast message: " + JSON.stringify(data));
-                        broadcastInvake(data);
+                        broadcastInvoke(data);
                         break;
 
                 }
             };
+
             window.socket.onclose = function (event) {
                 console.log("connection close!!!");
-                closeInvake(event);
+                closeInvoke(event);
             };
-            window.socket.onopen = function (event) {
-                console.log("connection success!!");
-                openInvake(event);
-            };
-        } else {
+
+
+        }
+        else
+        {
             alert("您的浏览器不支持WebSocket！！！");
         }
-    } else {
+    }
+    else
+    {
         $('#tipMsg').text("请输入昵称");
         $('#tipModal').modal('show');
     }
 }
 
-function send(auth, mess) {
-    if (!window.socket) {
+function sendAuthInfo()
+{
+    var obj = {};
+    obj.code = 10000;
+    obj.nick = $('#nick').val().trim();
+    send(true, JSON.stringify(obj));
+};
+
+function send(auth, mess)
+{
+    if (!window.socket)
+    {
         return;
     }
     if (socket.readyState == WebSocket.OPEN || auth) {
         console.log("send: " + mess);
         window.socket.send(mess);
-    } else {
+    }
+    else
+    {
         $('#tipMsg').text("连接没有成功，请重新登录");
         $('#tipModal').modal('show');
     }
 }
 ;
 
-function openInvake(event) {
-    var obj = {};
-    obj.code = 10000;
-    obj.nick = $('#nick').val().trim();
-    send(true, JSON.stringify(obj));
-}
-;
 
-
-function closeInvake(event) {
+function closeInvoke(event) {
     window.socket = null;
     window.isAuth = false;
     window.userCount = 0;
@@ -130,8 +153,10 @@ function closeInvake(event) {
  * 处理系统消息
  * @param data
  */
-function sysInvake(data) {
-    switch (data.extend.code) {
+function sysInvoke(data)
+{
+    switch (data.extend.code)
+    {
         case 20001: // user count
             console.log("current user: " + data.extend.mess);
             userCount = data.extend.mess;
@@ -140,51 +165,46 @@ function sysInvake(data) {
         case 20002: // auth
             console.log("auth result: " + data.extend.mess);
             isAuth = data.extend.mess;
-            if (isAuth) {
+            if (isAuth)
+            {
                 $("#menuModal").modal('hide');
                 $('#chatWin').show();
                 $('#content').append('欢迎来到嗨皮聊天室！！');
                 // $('#content').scrollTop($('#content')[0].scrollHeight);
             }
             break;
-        case 20003: // system message
-            console.log("system message: " + data.extend.mess);
+        case 20003: // system other message
+            console.log("system other message: " + data.extend.mess);
             break;
     }
-}
-;
+};
 
 /**
  * 处理广播消息
  * @param data
  */
-function broadcastInvake(data) {
+function broadcastInvoke(data) {
     var mess = data.body;
     var nick = data.extend.nick;
     var uid = data.extend.uid;
     var time = data.extend.time;
     mess = replace_em(mess);
-    var html = '<div class="title">'+nick+'&nbsp;('+uid+') &nbsp;'+time+'</div><div class="item">'+mess+'</div>';
+    var html = '<div class="title">' + nick + '&nbsp;(' + uid + ') &nbsp;' + time + '</div><div class="item">' + mess + '</div>';
     $("#content").append(html);
     $('#content').scrollTop($('#content')[0].scrollHeight);
 
 }
 ;
 
-function erorInvake(data) {
+function erorInvoke(data) {
 
-}
-;
+};
 
-/**
- * 处理ping消息
- * @param data
- */
-function pingInvake(data) {
+function pingInvoke(data) {
     //发送pong消息响应
     send(isAuth, "{'code':10016}");
-}
-;
+};
+
 //查看结果
 function replace_em(str) {
     str = str.replace(/\</g, '&lt;');
